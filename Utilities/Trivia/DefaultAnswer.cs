92467719203f42852692947615aabda423ec0a17
Gabriel Pasquale R Scavone <gabriel.prscavone@gmail.com>
2021-03-12 23:07:50 -0300

    Adding default answer and default question

---
+++ Utilities/Trivia/DefaultAnswer.cs
@@ -0,0 +1,18 @@
+﻿ using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DiscordBot.Utilities.Trivia
+{
+    class DefaultAnswer
+    {
+        public string Content { get; private set; }
+        public bool IsCorrect { get; private set; }
+
+        public DefaultAnswer(string content, bool isCorrect)
+        {
+            Content = content;
+            IsCorrect = isCorrect;
+        }
+    }
+}
---
+++ Utilities/Trivia/DefaultQuestion.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace DiscordBot.Utilities.Trivia
+{
+    class DefaultQuestion : IQuestion<DefaultAnswer>
+    {
+        public string Content {get; private set;}
+        public List<DefaultAnswer> Answers { get; private set; }
+
+        public Span<DefaultAnswer> GetAnswers() => new Span<DefaultAnswer>(Answers.ToArray());
+        
+        public DefaultAnswer GetCorrectAnswer() => Answers.Where((DefaultAnswer ans) => ans.IsCorrect == true).FirstOrDefault();
+
+        public string GetQuestion() => Content;
+
+        public bool IsCorrect(DefaultAnswer ans) => ans.IsCorrect;
+    }
+}
